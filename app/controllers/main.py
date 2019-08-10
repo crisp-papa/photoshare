b6e8682717f1f8d64d4bcad46017654f50b163f6
floatingeye <eriklougee@gmail.com>
2019-08-10 00:29:59 -0400

    Starting invite

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -3,8 +3,8 @@ from flask import render_template, flash
 
 @APP.route('/', methods=['GET'])
 def invite():
-    flash('error', 'error')
     flash('success!', 'success')
+    flash('error', 'error')
     flash('warning!?', 'warning')
     flash('just a message', 'message')
     return render_template('shared/main.html')
